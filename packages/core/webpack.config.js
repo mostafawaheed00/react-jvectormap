c819ed1688e689daaf41744dd30f6d3083a01646
Mostafa Waheed <waheedmostfa00@gmail.com>
2026-01-17 23:48:01 +0200

    feat: update npm client to npm, bump package version, enhance build script

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "./src/index.ts"),
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
